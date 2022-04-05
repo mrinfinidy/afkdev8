e853a02327d9e6189bc35557918e7fcdb009d97b
debby <mr.infinidy@protonmail.com>
2022-04-05 02:11:47 +0200

    Integrate with chakra-ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
     return (
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello I&apos;m afkdev8!
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        afkdev8
+                    </Heading>
+                    <p>Student in KA</p>
+                </Box>
+            </Box>
+        </Container>
     )
 }
 
