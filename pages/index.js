c7e445062ec7a4f0e13ef16dfe8fd59deafa00b4
debby <mr.infinidy@protonmail.com>
2022-04-06 21:40:43 +0200

    Page transitions with framer-motion

--- pages/index.js
+++ pages/index.js
@@ -9,101 +9,104 @@ import {
     useColorModeValue
 } from '@chakra-ui/react'
 import { ChevronRightIcon } from '@chakra-ui/icons'
+import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 
 const Page = () => {
     return (
-        <Container>
-            <Box
-                borderRadius="lg"
-                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-                p={3}
-                mb={6}
-                align="center"
-            >
-                Hello I&apos;m afkdev8!
-            </Box>
+        <Layout>
+            <Container>
+                <Box
+                    borderRadius="lg"
+                    bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+                    p={3}
+                    mb={6}
+                    align="center"
+                >
+                    Hello I&apos;m afkdev8!
+                </Box>
 
-            <Box display={{md:'flex'}}>
-                <Box flexGrow={1}>
-                    <Heading as="h2" variant="page-title">
-                        afkdev8
+                <Box display={{md:'flex'}}>
+                    <Box flexGrow={1}>
+                        <Heading as="h2" variant="page-title">
+                            afkdev8
+                        </Heading>
+                        <p>Student in KA</p>
+                    </Box>
+                    <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
+                        <Image
+                            borderColor="whiteAlpha.800"
+                            borderWidth={2}
+                            borderStyle="solid"
+                            maxWidth="100px"
+                            display="inline-block"
+                            borderRadius="full"
+                            src="/images/profile.jpg"
+                            alt="Profile Image"
+                        />
+                    </Box>
+                </Box>
+
+                <Section delay={0.1}>
+                    <Heading as="h3" variant="section-title">
+                        Work
                     </Heading>
-                    <p>Student in KA</p>
-                </Box>
-                <Box flexShrink={0} mt={{base: 4, md: 0}} ml={{md: 6}} align="center">
-                    <Image
-                        borderColor="whiteAlpha.800"
-                        borderWidth={2}
-                        borderStyle="solid"
-                        maxWidth="100px"
-                        display="inline-block"
-                        borderRadius="full"
-                        src="/images/profile.jpg"
-                        alt="Profile Image"
-                    />
-                </Box>
-            </Box>
+                    <Paragraph>
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                        <br></br>
+                        LINKS: {' '}
+                        <NextLink href="/works/stayAtHome">
+                            <Link>
+                                StayAtHome
+                            </Link>
+                        </NextLink>
+                    </Paragraph>
+                    <Box align="center" my={4}>
+                        <NextLink href="/works">
+                            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+                                My portfolio
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
 
-            <Section delay={0.1}>
-                <Heading as="h3" variant="section-title">
-                    Work
-                </Heading>
-                <Paragraph>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                    sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                    sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                    tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                    no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                    <br></br>
-                    LINKS: {' '}
-                    <NextLink href="/works/stayAtHome">
-                        <Link>
-                            StayAtHome
+                <Section delay={0.2}>
+                    <Heading as="h3" variant="section-title">
+                        Bio
+                    </Heading>
+                    <BioSection>
+                        <BioYear>19xx</BioYear>
+                        Born in ?, Europe.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2022</BioYear>
+                        Created this website.
+                    </BioSection>
+                </Section>
+
+                <Section delay={0.3}>
+                    <Heading as="h3" variant="section-title">
+                        ♡
+                    </Heading>
+                    <Paragraph>
+                        Ghibli,{' '}
+                        <Link href="https://www.youtube.com/watch?v=TKy0Lyl4g-s">
+                            Music
                         </Link>
-                    </NextLink>
-                </Paragraph>
-                <Box align="center" my={4}>
-                    <NextLink href="/works">
-                        <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                            My portfolio
-                        </Button>
-                    </NextLink>
-                </Box>
-            </Section>
-
-            <Section delay={0.2}>
-                <Heading as="h3" variant="section-title">
-                    Bio
-                </Heading>
-                <BioSection>
-                    <BioYear>19xx</BioYear>
-                    Born in ?, Europe.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2022</BioYear>
-                    Created this website.
-                </BioSection>
-            </Section>
-
-            <Section delay={0.3}>
-                <Heading as="h3" variant="section-title">
-                    ♡
-                </Heading>
-                <Paragraph>
-                    Ghibli,{' '}
-                    <Link href="https://www.youtube.com/watch?v=TKy0Lyl4g-s">
-                        Music
-                    </Link>
-                    , 1984, ...
-                </Paragraph>
-            </Section>
-        </Container>
+                        , 1984, ...
+                    </Paragraph>
+                </Section>
+            </Container>
+        </Layout>
     )
 }
 
--- pages/works.js
+++ pages/works.js
@@ -1,6 +1,7 @@
 import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import  Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
+import Layout from '../components/layouts/article'
 
 import thumbStayAtHome from '../public/images/works/thumbStayAtHome.png'
 import thumbYTSC from '../public/images/works/thumbYTSC.png'
@@ -9,35 +10,37 @@ import thumbAutoFocus from '../public/images/works/thumbAutoFocus.png'
 
 const Works = () => {
     return (
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
+        <Layout>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
+                    Works
+                </Heading>
 
-            <SimpleGrid columns={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem id="stayAtHome" title="StayAtHome" thumbnail={thumbStayAtHome}>
-                        An Android app that was developed during a Covid-Hackathon.
-                    </WorkGridItem>
-                </Section>
-                <Section>
-                    <WorkGridItem id="ytsChrome" title="YouTubeSearch Chrome" thumbnail={thumbYTSC}>
-                        A Chrome extension that adds right-click YouTube search to the browser.
-                    </WorkGridItem>
-                </Section>
-                
-                <Section delay={0.1}>
-                    <WorkGridItem id="ytsFirefox" title="YouTubeSearch Firefox" thumbnail={thumbYTSF}>
-                        A Firefox add-on that adds right-click YouTube search to the browser.
-                    </WorkGridItem>
-                </Section>
-                <Section delay={0.1}>
-                    <WorkGridItem id="autofocusYT" title="YouTube Autofocus" thumbnail={thumbAutoFocus}>
-                        A Firefox add-on that automatically focus the YouTube search bar.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem id="stayAtHome" title="StayAtHome" thumbnail={thumbStayAtHome}>
+                            An Android app that was developed during a Covid-Hackathon.
+                        </WorkGridItem>
+                    </Section>
+                    <Section>
+                        <WorkGridItem id="ytsChrome" title="YouTubeSearch Chrome" thumbnail={thumbYTSC}>
+                            A Chrome extension that adds right-click YouTube search to the browser.
+                        </WorkGridItem>
+                    </Section>
+
+                    <Section delay={0.1}>
+                        <WorkGridItem id="ytsFirefox" title="YouTubeSearch Firefox" thumbnail={thumbYTSF}>
+                            A Firefox add-on that adds right-click YouTube search to the browser.
+                        </WorkGridItem>
+                    </Section>
+                    <Section delay={0.1}>
+                        <WorkGridItem id="autofocusYT" title="YouTube Autofocus" thumbnail={thumbAutoFocus}>
+                            A Firefox add-on that automatically focus the YouTube search bar.
+                        </WorkGridItem>
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
     )
 }
 
