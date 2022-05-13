c7239f5e2d10ee9087d4beb5dc7057742f143c43
afkdev8 <mr.infinidy@protonmail.com>
2022-05-13 20:22:36 +0200

    Add Work description

--- pages/index.js
+++ pages/index.js
@@ -57,15 +57,14 @@ const Page = () => {
                         Work
                     </Heading>
                     <Paragraph>
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-                        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-                        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
-                        tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-                        At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
-                        no sea takimata sanctus est Lorem ipsum dolor sit amet.
-                        <br></br>
+                       	At the moment you can check out my hobby projects on the &quot;Works&quot; page.
+			Or just click on &quot;My portfolio&quot; below to go there.
+			They are mainly browser add-ons but there is also an Android app that was created
+			during a hackathon.
+			<br />
+			Maybe I can post something about my work in the future. It revolves around three.js and CAD,
+			more about it coming soon.
+			<br />
                         LINKS: {' '}
                         <NextLink href="/works/stayAtHome">
                             <Link>
