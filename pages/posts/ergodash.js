da0c12b0d027010e44f08975bbe2127455d425e9
afkdev8 <mr.infinidy@protonmail.com>
2022-12-10 00:46:43 +0100

    Write introduction text

--- pages/posts/ergodash.js
+++ pages/posts/ergodash.js
@@ -1,4 +1,4 @@
-import { Container, SimpleGrid } from '@chakra-ui/react'
+import { Container, SimpleGrid, Link } from '@chakra-ui/react'
 import { Title } from '../../components/post'
 import { GridItem } from '../../components/grid-item'
 import P from '../../components/paragraph'
@@ -14,27 +14,41 @@ const ErgoDash = () => {
 		<Layout title="">
 			<Container>
 				<Title>ErgoDash keyboard (work in progress)</Title>
-				<SimpleGrid columns={[1,1,2]} gap={6}>
-				<GridItem 
-					title="Switch case printed"
-					thumbnail={printCase}
-					href="/images/posts/printCase.jpg"
-				/>
-				<GridItem 
-					title="Diodes soldered"
-					thumbnail={diodesSoldered}
-					href="/images/posts/diodesSoldered.jpg"
-				/>
-				<GridItem 
-					title="Switches installed and soldered"
-					thumbnail={switchesInstalled}	
-					href="/images/posts/switchesInstalled.jpg"
-				/>
-				<GridItem 
-					title="Case assembled"
-					thumbnail={caseAssembled}
-					href="/images/posts/caseAssembled.jpg"
-				/>
+				<P>
+					<Link href="https://github.com/omkbd/ErgoDash">ErgoDash</Link>
+					is an open source split keyboard. I was introduced to it by a
+					generous friend when he gave me two ErgoDash PCBs and let me
+					try out the on he built. No need to say that I was amazed.
+				</P>	
+				<SimpleGrid columns={[1,1,2]} gap={6} my={4} >
+					<GridItem 
+						title=""
+						thumbnail={printCase}
+						href="/images/posts/printCase.jpg"
+					>
+						Switch case printed
+					</GridItem>
+					<GridItem 
+						title=""
+						thumbnail={diodesSoldered}
+						href="/images/posts/diodesSoldered.jpg"
+					>
+						Diodes soldered
+					</GridItem>
+					<GridItem 
+						title=""
+						thumbnail={switchesInstalled}	
+						href="/images/posts/switchesInstalled.jpg"
+					>
+						Switches installed
+					</GridItem>
+					<GridItem 
+						title=""
+						thumbnail={caseAssembled}
+						href="/images/posts/caseAssembled.jpg"
+					>
+						Case assembled
+					</GridItem>
 				</SimpleGrid>
 				<P>
 					ErgoDash keyboard placeholder text.
