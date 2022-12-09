ae3364cba82d17181d9f64800d6b5e281416720c
afkdev8 <mr.infinidy@protonmail.com>
2022-12-09 23:13:02 +0100

    Add ErgoDash page in Posts

--- pages/posts.js
+++ pages/posts.js
@@ -1,10 +1,12 @@
 import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
+import { GridItem, PostGridItem } from '../components/grid-item'
+import { ExternalLinkIcon } from '@chakra-ui/icons'
 
 import thumbDevAsLife from '../public/images/contents/devaslife.webp'
 import rambalacNagoya from '../public/images/contents/rambalac_nagoya.webp'
+import thumbErgoDash from '../public/images/posts/ergodash-1.jpg'
 
 const Posts = () => (
     <Layout title="">
@@ -24,6 +26,11 @@ const Posts = () => (
                         thumbnail={rambalacNagoya}
                         href="https://www.youtube.com/watch?v=w0SJgbALym8"
                     />
+					<PostGridItem 
+						id="ergodash"
+						title="ErgoDash (work in progress)"
+						thumbnail={thumbErgoDash}
+					/>
                 </SimpleGrid>
             </Section>
         </Container>
---
+++ pages/posts/ergodash.js
@@ -0,0 +1,19 @@
+import { Container } from '@chakra-ui/react'
+import { Title } from '../../components/post'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const ErgoDash = () => {
+	return (
+		<Layout title="">
+			<Container>
+				<Title>ErgoDash keyboard (work in progress)</Title>
+				<P>
+					ErgoDash keyboard placeholder text.
+				</P>
+			</Container>
+		</Layout>
+	)
+}
+
+export default ErgoDash
