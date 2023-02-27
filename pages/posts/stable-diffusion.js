4a508309d403b6d5b62cf7c13d28344324d90445
afkdev8 <mr.infinidy@protonmail.com>
2023-02-27 23:32:17 +0100

    Remove commented out code

--- pages/posts/stable-diffusion.js
+++ pages/posts/stable-diffusion.js
@@ -69,26 +69,6 @@ const StableDiffusion = () => {
 					&quot;parody&quot;. Although I have no idea what this meme could possibly mean
 					you can see a lot of christian elemnts in it. The text just seems to be gibberish.
 				</P>
-				{/*
-				<Heading as="h4" fontSize={13}>
-					Image 1: Mix between the pope and Bernie Sanders?
-				</Heading>
-				<Heading as="h4" fontSize={13}>
-					Image 2: An orthodox priest
-				</Heading>
-				<Heading as="h4" fontSize={13}>
-					Image 3: A poor person or maybe a monk?
-				</Heading>
-				<Heading as="h4" fontSize={13}>
-					Image 4: An angel
-				</Heading>
-				<Heading as="h4" fontSize={13}>
-					Image 5: Maybe the the white gown or the beard are supposed to look religous
-				</Heading>
-				<Heading as="h4" fontSize={13}>
-					Image 6: Ugly Mary?
-				</Heading>
-				*/}
 				<P>
 					Image 1: Mix between the pope and Bernie Sanders?
 				</P>
