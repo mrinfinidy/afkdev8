fc24e7c7318ccc49564be1fc8ee91be7bc6b052e
afkdev8 <mr.infinidy@protonmail.com>
2023-05-17 03:41:00 +0200

    Add page description

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,9 @@ export default class Document extends NextDocument {
     render () {
         return (
             <Html lang="en">
-                <Head />
+                <Head>
+					<meta name='description' content='afkdev8 Portfolio Page - This page presents my work and posts about stuff that I find intersting (AI, mechanical keyboards,...).' />
+				</Head>
                 <body>
                     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
                     <Main />
