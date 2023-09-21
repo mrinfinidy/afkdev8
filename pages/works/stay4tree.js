ab050edd7ad7f1bb5c41bfcdeb5c1d7480046994
afkdev8 <mr.infinidy@protonmail.com>
2023-09-21 03:05:04 +0200

    Embed YouTube video no Stay4Tree page

--- pages/works/stay4tree.js
+++ pages/works/stay4tree.js
@@ -1,8 +1,9 @@
-import { Container, Link, List, ListItem } from '@chakra-ui/react'
+import { Box, Container, Link, List, ListItem } from '@chakra-ui/react'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
+import YouTube from 'react-youtube'
 
 const Stay4Tree = () => {
     return (
@@ -15,7 +16,19 @@ const Stay4Tree = () => {
                     An Android app that was developed during a Covid quarantine hackathon. It encourages people to stay at home by rewarding their time at home with real trees beeing planted around the world. <br />
                     Unfortunately (but also fortunately) University resumed from its Covid-vacation right before I could finish this project and launch Stay4Tree.
                 </P>
+				<br />
+				<YouTube
+					videoId="Feny7UXY8NM"
+				/>
+				<br />
                 <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>YouTube</Meta>
+                        <Link 
+                            href="https://www.youtube.com/watch?v=Feny7UXY8NM" target="_blank">
+                            Link to YouTube <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
                     <ListItem>
                         <Meta>Website</Meta>
                         <Link 
@@ -23,12 +36,6 @@ const Stay4Tree = () => {
                             Here is a more detailed description of Stay4Tree <ExternalLinkIcon mx="2px" />
                         </Link>
                     </ListItem>
-					<ListItem>
-						<Meta>YouTube</Meta>
-						<Link href="https://www.youtube.com/watch?v=Feny7UXY8NM" target="_blank">
-							YouTube video describing the project <ExternalLinkIcon mx="2px" /> 
-						</Link>
-					</ListItem>
                     <ListItem>
                         <Meta>Platform</Meta>
                         <span>Android 5.0+</span>
