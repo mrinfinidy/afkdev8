f48293e1d863a8951fa26d05abc3f1c819967b12
afkdev8 <mr.infinidy@protonmail.com>
2023-09-23 22:52:47 +0200

    Custom Code tag

---
+++ components/code.js
@@ -0,0 +1,14 @@
+import { Code, useColorMode } from '@chakra-ui/react'
+
+const CodeStyled = ({ children, mIndent }) => {
+	const { colorMode } = useColorMode()
+	const colorScheme = colorMode == 'light' ? 'teal' : 'purple'
+
+	return (
+		<Code  colorScheme={ colorScheme } style={{ textIndent: mIndent }}>
+			{children}
+		</Code>
+	)
+}
+
+export default CodeStyled
