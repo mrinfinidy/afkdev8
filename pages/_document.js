bb6127d56c53120e6d0fbb92410132bba17962ee
afkdev8 <mr.infinidy@protonmail.com>
2024-10-23 09:45:18 +0200

    Add google verification meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default class Document extends NextDocument {
           <Html lang="en">
             <Head>
 					    <meta name='description' content='afkdev8 Portfolio Page - This page presents my work and posts about stuff that I find intersting (AI, mechanical keyboards,...).' />
+              <meta name="google-site-verification" content="aMWeTQTDOoDHXfcjaJe0iA9VUVS58X1XFOPD-A8xg7o" />
 				    </Head>
             <body>
               <ColorModeScript initialColorMode={theme.config.initialColorMode} />
