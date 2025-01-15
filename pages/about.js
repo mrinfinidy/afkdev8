838e7b089f1e65f5e820ae20e7fdf5b75f9ae951
afkdev8 <mr.infinidy@protonmail.com>
2025-01-15 01:30:33 +0100

    Add new logo
    new logo in dark and bright mode
    new logo in favicon
    add Marlis to Credits

--- pages/about.js
+++ pages/about.js
@@ -8,49 +8,60 @@ import Container4k from '../components/layouts/container'
 import { BulletSection, BulletPoint } from '../components/bullet'
 
 const About = () => (
-    <Layout title="">
-        <Container4k>
-            <Section delay={0.1}>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Credits
-                </Heading>
-                <BulletSection>
-                    <BulletPoint>
-                        <NextLink href="https://www.craftz.dog/" passHref={true}>
-                            <Link target="_blank" _focus={{ boxShadow: 'none' }}>
-                                &#64;craftzdog
-                            </Link>
-                        </NextLink>
-                    </BulletPoint>
-                    <br></br>
-                    Thank you for your amazing video guide. I have used it as a
-                    template to create this website.
-                </BulletSection>
-                <BulletSection>
-                    <BulletPoint>
-                        <NextLink href="https://www.cgtrader.com/jkielr" passHref={true}>
-                            <Link target="_blank" _focus={{ boxShadow: 'none' }}>
-                                Jesse Ragos
-                            </Link>
-                        </NextLink>
-                    </BulletPoint>
-                    <br></br>
-                    Thank you for your cute Totoro 3d model.
-                </BulletSection>
-            </Section>
+  <Layout title="">
+    <Container4k>
+      <Section delay={0.1}>
+        <Heading as="h3" fontSize={20} mb={4}>
+            Credits
+        </Heading>
+        <BulletSection>
+          <BulletPoint>
+            <NextLink href="https://www.instagram.com/marliscuts/" passHref={true}>
+              <Link target="_blank" _focus={{ boxShadow: 'none' }}>
+                &#64;marliscuts
+              </Link>
+            </NextLink>
+          </BulletPoint>
+          <br></br>
+          Thank you Marlis for designing my beautiful logo ♥
+        </BulletSection>
+        <BulletSection>
+          <BulletPoint>
+            <NextLink href="https://www.craftz.dog/" passHref={true}>
+              <Link target="_blank" _focus={{ boxShadow: 'none' }}>
+                &#64;craftzdog
+              </Link>
+            </NextLink>
+          </BulletPoint>
+          <br></br>
+          Thank you for your amazing video guide. I have used it as a
+          template to create this website.
+        </BulletSection>
+        <BulletSection>
+          <BulletPoint>
+            <NextLink href="https://www.cgtrader.com/jkielr" passHref={true}>
+                <Link target="_blank" _focus={{ boxShadow: 'none' }}>
+                  Jesse Ragos
+                </Link>
+            </NextLink>
+          </BulletPoint>
+          <br></br>
+          Thank you for your cute Totoro 3d model.
+        </BulletSection>
+      </Section>
 
-            <Section delay={0.2}>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    View Source
-                </Heading>
-                <NextLink href="https://github.com/mrinfinidy/afkdev8" passHref={true}>
-                    <Link target="_blank" _focus={{ boxShadow: 'none' }}>
-                        Source Code <ExternalLinkIcon mx="2px"/>
-                    </Link>
-                </NextLink>
-            </Section>
-        </Container4k>
-    </Layout>
+      <Section delay={0.2}>
+        <Heading as="h3" fontSize={20} mb={4}>
+          View Source
+        </Heading>
+        <NextLink href="https://github.com/mrinfinidy/afkdev8" passHref={true}>
+          <Link target="_blank" _focus={{ boxShadow: 'none' }}>
+            Source Code <ExternalLinkIcon mx="2px"/>
+          </Link>
+        </NextLink>
+      </Section>
+    </Container4k>
+  </Layout>
 )
 
 export default About
