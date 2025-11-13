c751f3e2152d4799852321bf8fdbbe710db68e98
afkdev8 <mr.infinidy@protonmail.com>
2025-11-13 03:00:55 +0100

    Add ransomware video to youtube post

--- pages/posts/youtube-channel.js
+++ pages/posts/youtube-channel.js
@@ -5,19 +5,26 @@ import Layout from '../../components/layouts/article'
 import Container4k from '../../components/layouts/container'
 
 const YoutubeChannel = () => {
-	return (
-		<Layout title="">
-			<Container4k>
-				<Title>My YouTube Channel</Title>
-				<P>
-					I post relaxing coding videos on&nbsp;
-          <Link href="https://www.youtube.com/channel/UCr2rewTU9FWA17Tft891-kw" target="_blank" _focus={{ boxShadow: 'none' }} >my YouTube channel</Link>
-					.
+  return (
+    <Layout title="">
+      <Container4k>
+        {' '}
+        <Title>My YouTube Channel</Title>
+        <P>
+          I post relaxing coding videos on&nbsp;
+          <Link
+            href="https://www.youtube.com/channel/UCr2rewTU9FWA17Tft891-kw"
+            target="_blank"
+            _focus={{ boxShadow: 'none' }}
+          >
+            my YouTube channel
+          </Link>
+          .
           <br />
           You can just watch and enjoy or follow along and use it as a guide.
           <br />
-          If there is enough interest I&apos;d also like to post a video about my desktop setup
-          or &quot;desktop rice&quot; at some point.
+          If there is enough interest I&apos;d also like to post a video about
+          my desktop setup or &quot;desktop rice&quot; at some point.
         </P>
         <br />
         <Heading as="h3" fontSize={20}>
@@ -26,10 +33,20 @@ const YoutubeChannel = () => {
         <br />
         <List>
           <ListItem>
-            <Link href="https://www.youtube.com/watch?v=LVJjFvGkAzs" target="_blank">
-                Terminal Timetable: Public transport timetable in Terminal using Python
+            <Link
+              href="https://www.youtube.com/watch?v=LVJjFvGkAzs"
+              target="_blank"
+            >
+              Terminal Timetable: Public transport timetable in Terminal using
+              Python
             </Link>
-            <Box position="relative" width="100%" height={{ base: "200px", md: "400px", lg: "700px" }} mt={2} mb={4}>
+            <Box
+              position="relative"
+              width="100%"
+              height={{ base: '200px', md: '400px', lg: '700px' }}
+              mt={2}
+              mb={4}
+            >
               <Box
                 as="iframe"
                 width="100%"
@@ -45,10 +62,39 @@ const YoutubeChannel = () => {
               />
             </Box>
           </ListItem>
+          <ListItem>
+            <Link
+              href="https://www.youtube.com/watch?v=djLVyRi5tts"
+              target="_blank"
+            >
+              Ransomware Simulation - How do they work?
+            </Link>
+            <Box
+              position="relative"
+              width="100%"
+              height={{ base: '200px', md: '400px', lg: '700px' }}
+              mt={2}
+              mb={4}
+            >
+              <Box
+                as="iframe"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/djLVyRi5tts?si=-Axk8EGoZIyulcfO"
+                title="Ransomware Simulation - How do they work?"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                position="absolute"
+                top="0"
+                left="0"
+              />
+            </Box>
+          </ListItem>
         </List>
-			</Container4k>
-		</Layout>
-	)
+      </Container4k>
+    </Layout>
+  )
 }
 
 export default YoutubeChannel
